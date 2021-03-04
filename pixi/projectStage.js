1a901ec64238090ac36fe447929ba106bead54fc
Leslie Meng <mengli26@gmail.com>
2021-03-04 10:59:35 -0500

    switch wheel to scroll

--- pixi/projectStage.js
+++ pixi/projectStage.js
@@ -106,7 +106,7 @@ function keyboard(value) {
 			helpButton.position.x = app.stage.pivot.x + appWidth - 35;
 		}
 	};
-	touchmove = (event) => {
+	function ontouchmove(event) {
 		event.preventDefault();
 		if (!lock[scroll]) {
 			if (
@@ -123,7 +123,7 @@ function keyboard(value) {
 			} else app.stage.pivot.x += window.pageXOffset || window.pageYOffset;
 			helpButton.position.x = app.stage.pivot.x + appWidth - 35;
 		}
-	};
+	}
 	/* resize - web resposive */
 	window.addEventListener('resize', resize);
 	function resize() {
@@ -141,15 +141,17 @@ function keyboard(value) {
 
 	window.addEventListener('keydown', downListener, false);
 	window.addEventListener('keyup', upListener, false);
-	window.addEventListener('wheel', _.throttle(onwheel, 0), false);
-	window.addEventListener('touchmove', touchmove, false);
+	// window.addEventListener('wheel', _.throttle(onwheel, 0), false);
+	window.addEventListener('touchmove', ontouchmove, false);
+	window.addEventListener('scroll', _.throttle(onwheel), false);
 
 	// Detach event listeners
 	key.unsubscribe = () => {
 		window.removeEventListener('keydown', downListener);
 		window.removeEventListener('keyup', upListener);
-		window.removeEventListener('wheel', _.throttle(onwheel, 0), false);
-		window.removeEventListener('touchmove', touchmove, false);
+		// window.removeEventListener('wheel', _.throttle(onwheel, 0), false);
+		window.removeEventListener('touchmove', ontouchmove, false);
+		window.removeEventListener('scroll', _.throttle(onwheel), false);
 	};
 
 	return key;
